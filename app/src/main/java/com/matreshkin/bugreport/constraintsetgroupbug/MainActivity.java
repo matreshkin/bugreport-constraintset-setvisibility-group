2f62835b53606b6a03faa122aff62029b16923d4
Vasiliy Polikarpov <polikarpov@yandex-team.ru>
2020-11-07 19:11:35 +0300

    second commit

--- app/src/main/java/com/matreshkin/bugreport/constraintsetgroupbug/MainActivity.java
+++ app/src/main/java/com/matreshkin/bugreport/constraintsetgroupbug/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // This fixes the behaviour
+        // setContentView(R.layout.activity_main_workaround);
 
         ConstraintLayout parent = findViewById(R.id.parent);
         parent.setOnClickListener(v -> toggleVisibility(parent));
