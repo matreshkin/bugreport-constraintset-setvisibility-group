02a3ced72b317af95fad2671e3839cd7d66f5fa7
Vasiliy Polikarpov <polikarpov@yandex-team.ru>
2020-11-07 18:47:19 +0300

    first commit

---
+++ app/src/main/java/com/matreshkin/bugreport/constraintsetgroupbug/MainActivity.java
@@ -0,0 +1,30 @@
+package com.matreshkin.bugreport.constraintsetgroupbug;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.constraintlayout.widget.Group;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ConstraintLayout parent = findViewById(R.id.parent);
+        parent.setOnClickListener(v -> toggleVisibility(parent));
+    }
+
+    private void toggleVisibility(ConstraintLayout parent) {
+        ConstraintSet constraintSet = new ConstraintSet();
+        View child = parent.findViewById(R.id.child);
+        int newVisibility = child.getVisibility() == View.GONE ? View.VISIBLE : View.GONE;
+        constraintSet.clone(parent);
+        constraintSet.setVisibility(R.id.group, newVisibility);
+        constraintSet.applyTo(parent);
+    }
+}
